da3ee0d15bda5777fd2a66844fae4a45bf84b3a1
Rachinder3 <rachindersingh@gmail.com>
2022-10-03 11:48:58 +0530

    Pipeline Trigger front end built

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
 
+from pyexpat.errors import messages
 from flask import Flask, jsonify, request, render_template
 
 
 import os, sys
 import json
 
-from phishing_domain_detection.logger import logging
+
 from phishing_domain_detection.exception import Phishing_Exception
 import os,sys
 from phishing_domain_detection.util.util import check
 from phishing_domain_detection.entity.phishing_estimator import PhishingEstimator
+from phishing_domain_detection.pipeline.pipeline import Pipeline
+from phishing_domain_detection.constants import EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME
 
-
-
+import pandas as pd
 # initializations
 app = Flask(__name__)
+
 model_directory = "saved_models"
+pipeline_folder = "phishing_domain_detection"
+artifacts_folder = "artifacts"
+
+experiment_file_path = os.path.join(pipeline_folder,artifacts_folder, EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME)
+rows_to_return = 5
 
 
+def generate_experiment_history_dict():
+    try:
+        #print(experiment_file_path)
+        if not os.path.exists(experiment_file_path):
+            return pd.DataFrame()
+        experiment_df = pd.read_csv(experiment_file_path)
+        
+        if 'experiment_file_path' in experiment_df.columns:
+            experiment_df.drop('experiment_file_path', axis = 1, inplace = True)
+        
+        if 'initialization_timestamp' in experiment_df.columns:
+            experiment_df.drop('initialization_timestamp', axis = 1, inplace = True)
+            
+        
+        experiment_df  = experiment_df.iloc[-1:(-1*rows_to_return)-1:-1]
+        #print(experiment_df)
+        
+        return experiment_df
+    except Exception as e:
+        print(str(e))
+    
+    
+    
 
 
 # views
@@ -128,10 +159,45 @@ def predictions_via_web():
         exception = Phishing_Exception(e,sys)
         print(exception.error_message)
     
+@app.route("/train", methods = ["GET","POST"])
+def train():
     
+    try:
+        context = None
+        
+        pipeline = Pipeline()
+        if request.method == "POST":
+            if Pipeline.experiment.running_status:
+                ## If pipeline already running, then dont start another pipeline
+                context = dict()
+                context["state"] = 2
+                message = "Pipeline is already running. Please wait some time before triggering another pipeline."
+                context["message"] = message
+            else:
+                context = dict()
+                context["state"] = 1
+                pipeline.start()
+                message = "Pipeline has been triggered."
+                context["message"] = message
+            
+            context["experiment"] = generate_experiment_history_dict()
+
+            return render_template("train.html", context = context)
+        return render_template("train.html", context=context)
+        
+    except Exception as e:
+        exception = Phishing_Exception(e,sys)
+        print(exception.error_message)
+        
+    
+    
+    
+    
+
     
     
     
 if __name__=='__main__':
     app.run()
+    #generate_experiment_history_dict()
     
